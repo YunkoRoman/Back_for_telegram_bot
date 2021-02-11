ac2b670adb475ac33fd8c1fc8e0f9102cc6d1ba9
Ev1lSoda <killer.be.29@gmail.com>
2021-02-11 13:39:35 +0200

    gitignore dist

--- dist/server.js
+++ dist/server.js
@@ -1,12 +1,14 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = __importDefault(require("http"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const http_1 = __importDefault(require('http'));
 // import RedisUser from './cache/redisUser';
-const app_1 = __importDefault(require("./app"));
-const logger_1 = __importDefault(require("./utils/logger"));
+const app_1 = __importDefault(require('./app'));
+const logger_1 = __importDefault(require('./utils/logger'));
 const PORT = process.env.PORT || 5000;
 console.log('PORT: ', PORT);
 const server = http_1.default.createServer(app_1.default());

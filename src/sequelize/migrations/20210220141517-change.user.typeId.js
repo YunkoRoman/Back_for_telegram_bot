f22dcc299685b68675816077b50bd6c472e424be
denys-bobrovnykov <62986552+denys-bobrovnykov@users.noreply.github.com>
2021-02-20 18:10:56 +0200

    Migration for user typeId change

---
+++ src/sequelize/migrations/20210220141517-change.user.typeId.js
@@ -0,0 +1,40 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.removeConstraint(
+        'chat_bot_users',
+        'chat_bot_users_typeId_fkey',
+        { transaction },
+      );
+      await queryInterface.changeColumn(
+        'chat_bot_users',
+        'typeId',
+        {
+          type: Sequelize.DataTypes.STRING(128),
+          allowNull: true,
+        },
+        { transaction },
+      );
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.removeConstriant(
+        'chat_bot_users',
+        'chat_bot_users_typeId_fkey',
+        { transaction },
+      );
+      await queryInterface.changeColumn('chat_bot_users', 'typeId', { transaction });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+};

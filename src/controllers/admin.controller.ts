d38096ad9cab8d462e24bcacefd5bac857d22729
YunkoRoman <romanyunko1996@gmail.com>
2021-02-23 22:18:30 +0200

    fixed bug

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,16 +1,15 @@
 /* eslint-disable no-unused-vars */
-import { Request, Response, NextFunction } from "express";
-import { getReasonPhrase, StatusCodes } from "http-status-codes";
-import FaqsService from "services/faqs.service";
-import { FaqModel } from "sequelize/models/faq.model";
-import UserService from "services/user.service";
-import UserTypesService from "services/user.types.service";
-import { UserTypeModel } from "sequelize/models/user.type.model";
-import UnansweredService from "services/unanswered.service";
-import { apiResponse, failedResponse, successResponse } from "../utils/response";
-import { logger } from "../utils/logger";
-import validator from "validator";
-import { customErrors } from "../errors/customErrors";
+import { Request, Response, NextFunction } from 'express';
+import { getReasonPhrase, StatusCodes } from 'http-status-codes';
+import FaqsService from 'services/faqs.service';
+import { FaqModel } from 'sequelize/models/faq.model';
+import UserService from 'services/user.service';
+import UserTypesService from 'services/user.types.service';
+import { UserTypeModel } from 'sequelize/models/user.type.model';
+import UnansweredService from 'services/unanswered.service';
+import { CONTACTS_FAQ_INTENT_NAME, FACULTY_FAQ_INTENT_NAME, UNIVERCITY_FAQ_INTENT_NAME } from '../types/types';
+import { apiResponse, failedResponse, successResponse } from '../utils/response';
+import { logger } from '../utils/logger';
 
 export default class AdminController {
   private faqsService: FaqsService;
@@ -21,7 +20,12 @@ export default class AdminController {
 
   private unansweredService: UnansweredService;
 
-  constructor(faqsService: FaqsService, userService: UserService, userTypesService: UserTypesService, unansweredService: UnansweredService) {
+  constructor(
+    faqsService: FaqsService,
+    userService: UserService,
+    userTypesService: UserTypesService,
+    unansweredService: UnansweredService,
+  ) {
     this.faqsService = faqsService;
     this.userService = userService;
     this.userTypesService = userTypesService;
@@ -30,14 +34,13 @@ export default class AdminController {
 
   public getMostPopularFaqs = async (req: Request, res: Response): Promise<Response> => {
     try {
-
-      logger.adminLogger.info("find most popular faqs");
+      logger.adminLogger.info('find most popular faqs');
       const result = await this.faqsService.getMostPopular();
       return apiResponse(res, successResponse(result), StatusCodes.OK);
     } catch (error) {
-      logger.adminLogger.error("unable to get most popular faqs", {
+      logger.adminLogger.error('unable to get most popular faqs', {
         Path: req.originalUrl,
-        meta: { ...error }
+        meta: { ...error },
       });
       logger.serverLogger.error(`Server error ${error.message}  CODE ${error.code}`);
       return apiResponse(res, failedResponse(error.message), StatusCodes.INTERNAL_SERVER_ERROR);
@@ -46,13 +49,13 @@ export default class AdminController {
 
   public getUnanswered = async (req: Request, res: Response): Promise<Response> => {
     try {
-      logger.adminLogger.info("getting all unanswered questions");
+      logger.adminLogger.info('getting all unanswered questions');
       const result = await this.unansweredService.getAllUnanswered();
       return apiResponse(res, successResponse(result), StatusCodes.OK);
     } catch (error) {
-      logger.adminLogger.error("unable to get unanswered", {
+      logger.adminLogger.error('unable to get unanswered', {
         Path: req.originalUrl,
-        meta: { ...error }
+        meta: { ...error },
       });
       logger.serverLogger.error(`Server error ${error.message}  CODE ${error.code}`);
       return apiResponse(res, failedResponse(error.message), StatusCodes.INTERNAL_SERVER_ERROR);
@@ -62,15 +65,16 @@ export default class AdminController {
   public editUniversityInfo = async (req: Request, res: Response): Promise<Response> => {
     const faqToEdit: FaqModel = req.body;
     // University qustion id
-    faqToEdit.id = 2;
+    faqToEdit.intentName = UNIVERCITY_FAQ_INTENT_NAME;
     try {
-      logger.adminLogger.info("edit university info", { Data: faqToEdit });
-      const result = await this.faqsService.updateFaqById(faqToEdit);
+      logger.adminLogger.info('edit university info', { Data: faqToEdit });
+      const result = await this.faqsService.updateFaqByIntentName(faqToEdit);
       return apiResponse(res, successResponse(result), StatusCodes.OK);
     } catch (error) {
-      logger.adminLogger.error("unable to edit university info", {
-        Path: req.originalUrl, Data: faqToEdit,
-        meta: { ...error }
+      logger.adminLogger.error('unable to edit university info', {
+        Path: req.originalUrl,
+        Data: faqToEdit,
+        meta: { ...error },
       });
       logger.serverLogger.error(`Server error ${error.message}  CODE ${error.code}`);
       return apiResponse(res, failedResponse(error.message), StatusCodes.INTERNAL_SERVER_ERROR);
@@ -80,15 +84,34 @@ export default class AdminController {
   public editFacultyInfo = async (req: Request, res: Response): Promise<Response> => {
     const faqToEdit: FaqModel = req.body;
     // Faculty qustion id
-    faqToEdit.id = 1;
+    faqToEdit.intentName = FACULTY_FAQ_INTENT_NAME;
     try {
-      logger.adminLogger.info("edit faculty info", { Data: faqToEdit });
-      const result = await this.faqsService.updateFaqById(faqToEdit);
+      logger.adminLogger.info('edit faculty info', { Data: faqToEdit });
+      const result = await this.faqsService.updateFaqByIntentName(faqToEdit);
       return apiResponse(res, successResponse(result), StatusCodes.OK);
     } catch (error) {
-      logger.adminLogger.error("unable to edit faculty info", {
-        Path: req.originalUrl, Data: faqToEdit,
-        meta: { ...error }
+      logger.adminLogger.error('unable to edit faculty info', {
+        Path: req.originalUrl,
+        Data: faqToEdit,
+        meta: { ...error },
+      });
+      logger.serverLogger.error(`Server error ${error.message}  CODE ${error.code}`);
+      return apiResponse(res, failedResponse(error.message), StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+  };
+
+  public editContactsInfo = async (req: Request, res: Response): Promise<Response> => {
+    const faqToEdit: FaqModel = req.body;
+    faqToEdit.intentName = CONTACTS_FAQ_INTENT_NAME;
+    try {
+      logger.adminLogger.info('edit contacts info', { Data: faqToEdit });
+      const result = await this.faqsService.updateFaqByIntentName(faqToEdit);
+      return apiResponse(res, successResponse(result), StatusCodes.OK);
+    } catch (error) {
+      logger.adminLogger.error('unable to edit contacts info', {
+        Path: req.originalUrl,
+        Data: faqToEdit,
+        meta: { ...error },
       });
       logger.serverLogger.error(`Server error ${error.message}  CODE ${error.code}`);
       return apiResponse(res, failedResponse(error.message), StatusCodes.INTERNAL_SERVER_ERROR);
@@ -96,15 +119,14 @@ export default class AdminController {
   };
 
   public refreshFaqs = async (req: Request, res: Response): Promise<Response> => {
-
     try {
-      logger.adminLogger.info("getting all faqs");
+      logger.adminLogger.info('getting all faqs');
       const result = await this.faqsService.getAllFaqs();
       return apiResponse(res, successResponse(result), StatusCodes.OK);
     } catch (error) {
-      logger.adminLogger.error("unable to get faqs", {
+      logger.adminLogger.error('unable to get faqs', {
         Path: req.originalUrl,
-        meta: { ...error }
+        meta: { ...error },
       });
       logger.serverLogger.error(`Server error ${error.message}  CODE ${error.code}`);
       return apiResponse(res, failedResponse(error.message), StatusCodes.INTERNAL_SERVER_ERROR);
@@ -114,13 +136,14 @@ export default class AdminController {
   public selectUsersByCategory = async (req: Request, res: Response): Promise<Response> => {
     const { type } = req.body;
     try {
-      logger.adminLogger.info("select users by type", { Data: type });
+      logger.adminLogger.info('select users by type', { Data: type });
       const result = await this.userService.getAllUsersByType(type);
       return apiResponse(res, successResponse(result), StatusCodes.OK);
     } catch (error) {
-      logger.adminLogger.error("unable select users by type", {
-        Path: req.originalUrl, Data: type,
-        meta: { ...error }
+      logger.adminLogger.error('unable select users by type', {
+        Path: req.originalUrl,
+        Data: type,
+        meta: { ...error },
       });
       logger.serverLogger.error(`Server error ${error.message}  CODE ${error.code}`);
       return apiResponse(res, failedResponse(error.message), StatusCodes.INTERNAL_SERVER_ERROR);
@@ -129,13 +152,13 @@ export default class AdminController {
 
   public getAllUserTypes = async (req: Request, res: Response): Promise<Response> => {
     try {
-      logger.adminLogger.info("get all user types");
+      logger.adminLogger.info('get all user types');
       const faqs = await this.userTypesService.getAllUserTypes();
       return apiResponse(res, successResponse(faqs), StatusCodes.OK);
     } catch (error) {
-      logger.adminLogger.error("error while getting all user types", {
+      logger.adminLogger.error('error while getting all user types', {
         Path: req.originalUrl,
-        meta: { ...error }
+        meta: { ...error },
       });
       logger.serverLogger.error(`Server error ${error.message}  CODE ${error.code}`);
 
@@ -146,13 +169,14 @@ export default class AdminController {
   public addNewType = async (req: Request, res: Response): Promise<Response> => {
     const type: UserTypeModel = req.body;
     try {
-      logger.adminLogger.info("add new category(type)", { Data: type });
+      logger.adminLogger.info('add new category(type)', { Data: type });
       const result = await this.userTypesService.addNewUserType(type);
       return apiResponse(res, successResponse(result), StatusCodes.OK);
     } catch (error) {
-      logger.adminLogger.error("unable select users by type", {
-        Path: req.originalUrl, Data: type,
-        meta: { ...error }
+      logger.adminLogger.error('unable select users by type', {
+        Path: req.originalUrl,
+        Data: type,
+        meta: { ...error },
       });
       logger.serverLogger.error(`Server error ${error.message}  CODE ${error.code}`);
 
